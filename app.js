749c695326cce40f3f2cb0dcfa3cb72c868168e6
Oxide <mohammedhaider15mo@gmail.com>
2024-08-27 18:11:54 +0300

    STYLEEEEE

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(
 );
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/", indexRouter);
 
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -4,6 +4,7 @@ import PostsController from "../controller/PostsController.js";
 const router = express.Router();
 
 router.get("/new", PostsController.newPostGet);
+router.post("/new", PostsController.newPost_Post);
 router.get("/delete/:postId", PostsController.deletePostGet);
 
 export { router as postsRouter };
